d8a20d0a0f0a2e3d06aa242e1de1e381f1f79a63
dfa-htwsaar <abawi@htwsaar.de>
2021-06-11 20:47:56 +0200

    first version

---
+++ u03/operators_relational.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#   Example from courses made by Prof. Dr. Daniel F. Abawi and Michael B. Schmidt
+#   htw saar, Saarbrücken, Germany
+
+# less than
+print('3 < 4 is', 3 < 4)
+
+# less than or equal to
+print('5 <= 6 is', 5 <= 6)
+
+# greater than
+print('3 > 4 is', 3 > 4)
+
+# greater than or equal to
+print('5 >= 6 is', 5 >= 6)
+
+# equivalence
+print('3 == 4 is', 3 == 4)
+
+# not equal
+print('3 != 4 is', 3 != 4)
