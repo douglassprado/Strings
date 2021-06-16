2f7fc65029a9a61d8462b1af8f7a75ecb7922f3a
Michael B. Schmidt <mibschmidt1975@gmail.com>
2021-06-17 00:04:58 +0200

    Add an example for if-statement

---
+++ u04/ex_basic_if_statement.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# This example script shows how to use a basic if-statement
+#create a variable 
+x=3
+
+#check if x has a value of 3
+if x==3:
+  #In this block we know that x has a value of three 
+  #so we print a message
+  print("x has a value of 3")
+
