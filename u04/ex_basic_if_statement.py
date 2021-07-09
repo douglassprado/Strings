10b84ee778653a520e7ba66d0a6ea4b08abc6563
dfa-htwsaar <abawi@htwsaar.de>
2021-07-09 19:26:50 +0200

    correction

--- u04/ex_basic_if_statement.py
+++ u04/ex_basic_if_statement.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+#   Example from courses made by Prof. Dr. Daniel F. Abawi and Michael B. Schmidt
+#   htw saar, Saarbrücken, Germany
+
 # This example script shows how to use a basic if-statement
-#create a variable 
-x=3
+# create a variable
+x = 3
 
-#check if x has a value of 3
-if x==3:
-  #In this block we know that x has a value of three 
-  #so we print a message
-  print("x has a value of 3")
-
+# check if x has a value of 3
+if x == 3:
+    # In this block we know that x has a value of three
+    # so we print a message
+    print("x has a value of 3")
